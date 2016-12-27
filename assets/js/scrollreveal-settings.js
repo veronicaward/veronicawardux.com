0f0b92b41d3a90b1a2d1aabfff7667393c1053eb
Alan Vezina <alan@fancysandwiches.com>
2016-12-26 21:00:54 -0800

    Add scrollreveal

---
+++ assets/js/scrollreveal-settings.js
@@ -0,0 +1,61 @@
+var sr = ScrollReveal();
+
+sr.reveal('.zoom-in', {
+  duration: 1000,
+  reset: true
+});
+
+sr.reveal('.fade-ltr', {
+  duration: 1000,
+  reset: true,
+  origin: 'left',
+  distance: '60px',
+  scale: 1
+});
+
+sr.reveal('.fade-rtl', {
+  duration: 1000,
+  reset: true,
+  origin: 'right',
+  distance: '60px',
+  scale: 1
+});
+
+sr.reveal('.delay-100', {
+  delay: 100
+});
+
+
+sr.reveal('.delay-200', {
+  delay: 200
+});
+
+
+sr.reveal('.delay-300', {
+  delay: 300
+});
+
+
+sr.reveal('.delay-400', {
+  delay: 400
+});
+
+
+sr.reveal('.delay-500', {
+  delay: 500
+});
+
+
+sr.reveal('.delay-600', {
+  delay: 600
+});
+
+
+sr.reveal('.delay-700', {
+  delay: 700
+});
+
+
+sr.reveal('.delay-800', {
+  delay: 800
+});
